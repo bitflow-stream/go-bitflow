fca3445999ce00d4c6412849c17d2318bd0e09b5
Anton Gulenko <anton.gulenko@googlemail.com>
2016-03-02 11:02:08 +0100

    added compiled binary

--- make-all.sh
+++ make-all.sh
@@ -4,3 +4,4 @@ path=$(go list -e -f '{{.Dir}}' github.com/citlab/monitoring)
 cd "$path"
 gpm
 go install github.com/citlab/monitoring/data-collection-agent
+cp $(which data-collection-agent) bin/data-collection-agent
