d645c2a746f62bb5ed48b2c16297b2e01311372f
Anton Gulenko <anton.gulenko@googlemail.com>
2016-03-18 00:21:10 +0100

    make script and binary

--- make-all.sh
+++ make-all.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-# === Package depedencies
-dpkg -l libvirt-dev &> /dev/null || sudo apt-get install -y libvirt-dev
+if [ "$1" = "install" ]; then
+    # === Package depedencies
+    dpkg -l libvirt-dev &> /dev/null || sudo apt-get install -y libvirt-dev
 
-# === Go dependencies
-path=$(go list -e -f '{{.Dir}}' github.com/citlab/monitoring)
-cd "$path"
-gpm
+    # === Go dependencies
+    path=$(go list -e -f '{{.Dir}}' github.com/citlab/monitoring)
+    cd "$path"
+    gpm
+fi
 
 # === Build & Install
 go install github.com/citlab/monitoring/data-collection-agent
