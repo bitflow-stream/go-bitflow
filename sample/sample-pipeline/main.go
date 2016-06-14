406e3f465f19163703677d928568b326563a5a47
Anton Gulenko <anton.gulenko@googlemail.com>
2016-06-14 11:48:33 +0200

    Using sample-pipeline instead of analysis-pipeline.

--- sample/sample-pipeline/main.go
+++ sample/sample-pipeline/main.go
@@ -8,14 +8,20 @@ import (
 	"github.com/antongulenko/golib"
 )
 
+var handlePipeline func(p *sample.CmdSamplePipeline)
+
 // Example pipeline with just the basic input and output parts (no data processing)
 // This can read data from a source and output it to one or more sinks.
+// Setting handlePipeline can be used e.g. to add SampleProcessors to the pipeline.
 func do_main() int {
 	var p sample.CmdSamplePipeline
 	p.ParseFlags()
 	flag.Parse()
 	defer golib.ProfileCpu()()
 	p.Init()
+	if handlePipeline != nil {
+		handlePipeline(&p)
+	}
 	return p.StartAndWait()
 }
 
