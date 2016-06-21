4326b6b2903dc72424b0e9f998f0faff96dba199
Anton Gulenko <anton.gulenko@googlemail.com>
2016-06-21 14:48:12 +0200

    Added separate binary for prototype data aggregator

---
+++ prototype-data-aggregator/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/antongulenko/data2go/sample"
+	"github.com/antongulenko/golib"
+)
+
+var readSampleHandler sample.ReadSampleHandler
+var handlePipeline func(p *sample.CmdSamplePipeline)
+
+// Example pipeline with just the basic input and output parts (no data processing)
+// This can read data from a source and output it to one or more sinks.
+// Setting handlePipeline can be used e.g. to add SampleProcessors to the pipeline.
+func do_main() int {
+	var p sample.CmdSamplePipeline
+	p.ParseFlags()
+	flag.Parse()
+	defer golib.ProfileCpu()()
+	p.ReadSampleHandler = readSampleHandler
+	p.Init()
+	if handlePipeline != nil {
+		handlePipeline(&p)
+	}
+	return p.StartAndWait()
+}
+
+func main() {
+	os.Exit(do_main())
+}
