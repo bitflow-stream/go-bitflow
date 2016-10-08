fe64430b80674ab5ec600fae89b9ad2968c169fa
Anton Gulenko <anton.gulenko@googlemail.com>
2016-10-08 12:18:01 +0200

    Added locking for accessing tags of a sample

--- sample/sample.go
+++ sample/sample.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -42,78 +43,109 @@ type Sample struct {
 	Values []Value
 	Time   time.Time
 
+	tagsLock    sync.RWMutex
 	tags        map[string]string
 	orderedTags []string // All keys from tags, with consistent ordering
 }
 
-func (sample *Sample) HasTag(name string) bool {
-	_, ok := sample.tags[name]
-	return ok
+func (sample *Sample) lockRead(do func()) {
+	sample.tagsLock.RLock()
+	defer sample.tagsLock.RUnlock()
+	do()
+}
+
+func (sample *Sample) lockWrite(do func()) {
+	sample.tagsLock.Lock()
+	defer sample.tagsLock.Unlock()
+	do()
+}
+
+func (sample *Sample) HasTag(name string) (ok bool) {
+	sample.lockRead(func() {
+		_, ok = sample.tags[name]
+	})
+	return
 }
 
 func (sample *Sample) SetTag(name, value string) {
-	if sample.tags == nil {
-		sample.tags = make(map[string]string)
-	}
-	if _, exists := sample.tags[name]; !exists {
-		sample.orderedTags = append(sample.orderedTags, name)
-		sort.Strings(sample.orderedTags)
-	}
-	sample.tags[name] = value
+	sample.lockWrite(func() {
+		if sample.tags == nil {
+			sample.tags = make(map[string]string)
+		}
+		if _, exists := sample.tags[name]; !exists {
+			sample.orderedTags = append(sample.orderedTags, name)
+			sort.Strings(sample.orderedTags)
+		}
+		sample.tags[name] = value
+	})
 }
 
 func (sample *Sample) DeleteTag(name string) {
-	if _, ok := sample.tags[name]; ok {
-		l := len(sample.orderedTags)
-		index := sort.SearchStrings(sample.orderedTags, name)
-		if index < l && index >= 0 && sample.orderedTags[index] == name {
-			// Delete element at index
-			sample.orderedTags = append(sample.orderedTags[:index], sample.orderedTags[index+1:]...)
+	sample.lockWrite(func() {
+		if _, ok := sample.tags[name]; ok {
+			l := len(sample.orderedTags)
+			index := sort.SearchStrings(sample.orderedTags, name)
+			if index < l && index >= 0 && sample.orderedTags[index] == name {
+				// Delete element at index
+				sample.orderedTags = append(sample.orderedTags[:index], sample.orderedTags[index+1:]...)
+			}
+			delete(sample.tags, name)
 		}
-		delete(sample.tags, name)
-	}
+	})
 }
 
-func (sample *Sample) Tag(name string) string {
-	return sample.tags[name]
+func (sample *Sample) Tag(name string) (value string) {
+	sample.lockRead(func() {
+		value = sample.tags[name]
+	})
+	return
 }
 
-func (sample *Sample) NumTags() int {
-	return len(sample.tags)
+func (sample *Sample) NumTags() (l int) {
+	sample.lockRead(func() {
+		l = len(sample.tags)
+	})
+	return
 }
 
-func (sample *Sample) TagString() string {
-	var b bytes.Buffer
-	started := false
-	for _, key := range sample.orderedTags {
-		value := sample.tags[key]
-		if started {
-			b.Write([]byte(tag_separator))
+func (sample *Sample) TagString() (res string) {
+	sample.lockRead(func() {
+		var b bytes.Buffer
+		started := false
+		for _, key := range sample.orderedTags {
+			value := sample.tags[key]
+			if started {
+				b.Write([]byte(tag_separator))
+			}
+			b.Write([]byte(escapeTagString(key)))
+			b.Write([]byte(tag_equals))
+			b.Write([]byte(escapeTagString(value)))
+			started = true
 		}
-		b.Write([]byte(escapeTagString(key)))
-		b.Write([]byte(tag_equals))
-		b.Write([]byte(escapeTagString(value)))
-		started = true
-	}
-	return b.String()
+		res = b.String()
+	})
+	return
 }
 
 func escapeTagString(str string) string {
 	return tagStringEscaper.Replace(str)
 }
 
-func (sample *Sample) ParseTagString(tags string) error {
-	sample.tags = make(map[string]string)
-	fields := strings.FieldsFunc(tags, func(r rune) bool {
-		return r == tag_equals_rune || r == tag_separator_rune
+func (sample *Sample) ParseTagString(tags string) (err error) {
+	sample.lockWrite(func() {
+		sample.tags = make(map[string]string)
+		fields := strings.FieldsFunc(tags, func(r rune) bool {
+			return r == tag_equals_rune || r == tag_separator_rune
+		})
+		if len(fields)%2 == 1 {
+			err = fmt.Errorf("Illegal tags string: %v", tags)
+			return
+		}
+		for i := 0; i < len(fields); i += 2 {
+			sample.SetTag(fields[i], fields[i+1])
+		}
 	})
-	if len(fields)%2 == 1 {
-		return fmt.Errorf("Illegal tags string: %v", tags)
-	}
-	for i := 0; i < len(fields); i += 2 {
-		sample.SetTag(fields[i], fields[i+1])
-	}
-	return nil
+	return
 }
 
 // This must be called in the Sample() method of every MetricSink implementation
@@ -126,13 +158,15 @@ func (sample *Sample) Check(header Header) error {
 }
 
 func (sample *Sample) CopyMetadataFrom(other Sample) {
-	sample.Time = other.Time
-	sample.tags = make(map[string]string, len(other.tags))
-	sample.orderedTags = make([]string, len(other.orderedTags))
-	copy(sample.orderedTags, other.orderedTags)
-	for key, val := range other.tags {
-		sample.tags[key] = val
-	}
+	sample.lockWrite(func() {
+		sample.Time = other.Time
+		sample.tags = make(map[string]string, len(other.tags))
+		sample.orderedTags = make([]string, len(other.orderedTags))
+		copy(sample.orderedTags, other.orderedTags)
+		for key, val := range other.tags {
+			sample.tags[key] = val
+		}
+	})
 }
 
 // All metadata is copied deeply, but values are referencing the old values
