f697287174f4967d53bc08c1f3a30ab9521c47c4
Anton Gulenko <anton.gulenko@googlemail.com>
2016-10-12 12:17:09 +0200

    Added analsis for printing timeline histogram of samples.

--- analysis/analysis-pipeline/misc.go
+++ analysis/analysis-pipeline/misc.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"math"
+	"sort"
+	"strconv"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -14,6 +18,7 @@ func init() {
 	RegisterAnalysisParams("print_tags", print_tags, "tag to print")
 	RegisterAnalysisParams("count_tags", count_tags, "tag to count")
 	RegisterAnalysis("print_timerange", print_timerange)
+	RegisterAnalysisParams("print_timeline", print_timeline, "number of buckets for the timeline-histogram") // Print a timeline showing a rudimentary histogram of the number of samples
 }
 
 type UniqueTagPrinter struct {
@@ -129,4 +134,79 @@ func (printer *TimerangePrinter) String() string {
 
 func print_timerange(p *SamplePipeline) {
 	p.Add(new(TimerangePrinter))
+}
+
+type TimelinePrinter struct {
+	NumBuckets uint64
+}
+
+func (p *TimelinePrinter) String() string {
+	return fmt.Sprintf("Print timeline (len %v)", p.NumBuckets)
+}
+
+func (p *TimelinePrinter) ProcessBatch(header *sample.Header, samples []*sample.Sample) (*sample.Header, []*sample.Sample, error) {
+	var from time.Time
+	var to time.Time
+	for _, sample := range samples {
+		t := sample.Time
+		if from.IsZero() || to.IsZero() {
+			from = t
+			to = t
+		} else if t.Before(from) {
+			from = t
+		} else if t.After(to) {
+			to = t
+		}
+	}
+	duration := to.Sub(from)
+	bucketDuration := duration / time.Duration(p.NumBuckets)
+	buckets := make([]int, p.NumBuckets)
+	bucketEnds := make([]time.Time, p.NumBuckets)
+	for i := uint64(0); i < p.NumBuckets-1; i++ {
+		bucketEnds[i] = from.Add(time.Duration(i+1) * bucketDuration)
+	}
+	bucketEnds[p.NumBuckets-1] = to // No rounding error
+	for _, sample := range samples {
+		index := sort.Search(len(buckets), func(n int) bool {
+			return !sample.Time.After(bucketEnds[n])
+		})
+		if index == len(buckets) {
+			log.Fatalln("WRONG:", sample.Time, "FIRST:", bucketEnds[0], "LAST:", bucketEnds[len(bucketEnds)-1], "START:", from, "END:", to)
+		}
+		buckets[index]++
+	}
+	largestBuffer := 0
+	for _, num := range buckets {
+		if num > largestBuffer {
+			largestBuffer = num
+		}
+	}
+	var timeline bytes.Buffer
+	for _, bucketSize := range buckets {
+		if bucketSize == 0 {
+			timeline.WriteRune('-')
+		} else {
+			num := int(math.Ceil(float64(bucketSize)/float64(largestBuffer)*10)) - 1 // [0..9]
+			timeline.WriteString(strconv.Itoa(num))
+		}
+	}
+
+	log.Println("[Timeline]: Start:", from)
+	log.Println("[Timeline]: End:", to)
+	log.Println("[Timeline]: Duration:", duration)
+	log.Println("[Timeline]: One bucket:", bucketDuration)
+	log.Println("[Timeline]:", timeline.String())
+	return header, samples, nil
+}
+
+func print_timeline(p *SamplePipeline, param string) {
+	buckets, err := strconv.ParseUint(param, 10, 64)
+	if err != nil {
+		log.Warnln("Failed to parse parameter for -e print_timeline:", err)
+		buckets = 10
+	}
+	if buckets == 0 {
+		buckets = 1
+	}
+	p.Batch(&TimelinePrinter{NumBuckets: buckets})
 }
