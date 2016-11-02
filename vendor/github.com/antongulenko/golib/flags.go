a1e932a97f8c0577dbebcd4d2af8e7e871ee3e74
Anton Gulenko <anton.gulenko@googlemail.com>
2016-11-02 13:41:27 +0100

    Updated vendored dependencies

--- vendor/github.com/antongulenko/golib/flags.go
+++ vendor/github.com/antongulenko/golib/flags.go
@@ -1,6 +1,9 @@
 package golib
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type StringSlice []string
 
@@ -10,5 +13,26 @@ func (i *StringSlice) String() string {
 
 func (i *StringSlice) Set(value string) error {
 	*i = append(*i, value)
+	return nil
+}
+
+const KeyValueSeparator = "="
+
+type KeyValueStringSlice struct {
+	Keys   []string
+	Values []string
+}
+
+func (i *KeyValueStringSlice) String() string {
+	return fmt.Sprintf("%v", *i)
+}
+
+func (i *KeyValueStringSlice) Set(value string) error {
+	parts := strings.SplitN(value, KeyValueSeparator, 2)
+	if len(parts) != 2 {
+		return fmt.Errorf("Wrong format. Need key=value, got " + value)
+	}
+	i.Keys = append(i.Keys, parts[0])
+	i.Values = append(i.Values, parts[1])
 	return nil
 }
