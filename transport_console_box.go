335a103676b1000cb395d9bf53166774f923eb4d
Anton Gulenko <anton.gulenko@googlemail.com>
2016-12-03 23:08:22 +0100

    added missing comments

--- transport_console_box.go
+++ transport_console_box.go
@@ -10,11 +10,20 @@ import (
 	"github.com/antongulenko/golib/gotermBox"
 )
 
-// TODO comment the methods and types in this file
-
+// ConsoleBoxSink implements the MetricSink interface by printing the received
+// samples to the standard out. Contrary to the ConsoleSink, the screen is erased
+// before printing a new sample, and the output is embedded in a box that shows
+// the last lines of log output at the bottom. ConsoleBoxSink does not implement
+// MarshallingMetricSink, because it uses its own, fixed marshaller.
+//
+// Multiple fields provide access to configuration options.
 type ConsoleBoxSink struct {
 	AbstractMetricSink
+
+	// CliLogBox provides configuration options for the command-line box
 	gotermBox.CliLogBox
+
+	// UpdateInterval gives the wait-period between screen-refresh cycles.
 	UpdateInterval time.Duration
 
 	updateTask *golib.LoopTask
@@ -29,10 +38,14 @@ func (sink *ConsoleBoxSink) Init() {
 	sink.RegisterMessageHook()
 }
 
+// String implements the MetricSink interface.
 func (sink *ConsoleBoxSink) String() string {
 	return "ConsoleBoxSink"
 }
 
+// Start implements the MetricSink interface. It starts a goroutine
+// that regularly refreshes the screen to display the current sample values
+// and latest log output lines.
 func (sink *ConsoleBoxSink) Start(wg *sync.WaitGroup) golib.StopChan {
 	sink.InterceptLogger()
 	log.Println("Printing samples to table")
@@ -69,10 +82,15 @@ func (sink *ConsoleBoxSink) updateBox() (err error) {
 	return
 }
 
+// Close implements the MetricSink interface. It stops the screen refresh
+// goroutine.
 func (sink *ConsoleBoxSink) Close() {
 	sink.updateTask.Stop()
 }
 
+// Sample implements the MetricSink interface. The latest sample is stored
+// and displayed on the console on the next screen refresh. Intermediate
+// samples might get lost without being displayed.
 func (sink *ConsoleBoxSink) Sample(sample *Sample, header *Header) error {
 	if err := sample.Check(header); err != nil {
 		return err
