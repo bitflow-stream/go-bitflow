d26422b24e7cd6c1e5b1c9c6c77d3efdd248ad40
Seema-n88 <seemabharadwaj29@gmail.com>
2018-07-31 13:50:50 +0200

    test case added for update

--- clustering/denstream/cluster-space-birchtree_test.go
+++ clustering/denstream/cluster-space-birchtree_test.go
@@ -96,3 +96,41 @@ func (s *birchTestSuite) TestNearestCluster() {
 	ass.Equal(testnearestCluster, nearestCluster.Center())
 
 }
+
+func (s *birchTestSuite) TestUpdateCluster() {
+
+	ass := assert.New(s.t)
+	clusterSpace := NewBirchTreeClusterSpace(2)
+	var added = [][]float64{[]float64{-0.41527107552819653, -0.14652729027863676},
+		[]float64{-0.376650025446918, -0.1465249275808625},
+		[]float64{-0.6067124276495246, -0.14815727290903336},
+		[]float64{-0.6129442509054625, -0.12516007473218244}}
+
+	testpoint := []float64{-0.6035847296436938, 0.10782419498799414}
+	for _, point := range added {
+		clusterSpace.NewCluster(point, time.Now())
+	}
+
+	ass.Equal(clusterSpace.root.numChildren, 2)
+	ass.Equal(clusterSpace.root.Coreset.W(), 4.0)
+
+	nearestCluster := clusterSpace.NearestCluster(testpoint)
+
+	testnearestCluster := testpoint
+	dist := 0.0
+	for _, cluster := range added {
+		d := clustering.EuclideanDistance(cluster, testpoint)
+		if dist == 0.0 || d < dist {
+			testnearestCluster = cluster
+		}
+
+	}
+
+	ass.Equal(testnearestCluster, nearestCluster.Center())
+
+	clusterSpace.UpdateCluster(nearestCluster, func() bool {
+		nearestCluster.Merge(testpoint)
+		return true
+	})
+
+}
