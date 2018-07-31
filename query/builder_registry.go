eb963c8a4f6a0ce274714829b006413e87ca2d8b
Anton Gulenko <anton.gulenko@googlemail.com>
2018-07-31 13:57:53 +0200

    Added convenience functions to parse processor step parameters

--- query/builder_registry.go
+++ query/builder_registry.go
@@ -2,6 +2,8 @@ package query
 
 import (
 	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/antongulenko/go-bitflow-pipeline"
 	"github.com/antongulenko/go-bitflow-pipeline/fork"
@@ -23,6 +25,80 @@ type ForkFunc func(subpiplines []Subpipeline, params map[string]string) (fork.Fo
 
 func ParameterError(name string, err error) error {
 	return fmt.Errorf("Failed to parse '%v' parameter: %v", name, err)
+}
+
+func StrParam(params map[string]string, name string, defaultVal string, hasDefault bool, err *error) string {
+	if *err != nil {
+		return ""
+	}
+	strVal, ok := params[name]
+	if ok {
+		return strVal
+	} else if hasDefault {
+		return defaultVal
+	} else {
+		*err = ParameterError(name, fmt.Errorf("Missing required parmeter"))
+		return ""
+	}
+}
+
+func DurationParam(params map[string]string, name string, defaultVal time.Duration, hasDefault bool, err *error) time.Duration {
+	if *err != nil {
+		return 0
+	}
+	strVal, ok := params[name]
+	if ok {
+		parsed, parseErr := time.ParseDuration(strVal)
+		if parseErr != nil {
+			*err = ParameterError(name, parseErr)
+			return 0
+		}
+		return parsed
+	} else if hasDefault {
+		return defaultVal
+	} else {
+		*err = ParameterError(name, fmt.Errorf("Missing required parmeter"))
+		return 0
+	}
+}
+
+func IntParam(params map[string]string, name string, defaultVal int, hasDefault bool, err *error) int {
+	if *err != nil {
+		return 0
+	}
+	strVal, ok := params[name]
+	if ok {
+		parsed, parseErr := strconv.Atoi(strVal)
+		if parseErr != nil {
+			*err = ParameterError(name, parseErr)
+			return 0
+		}
+		return parsed
+	} else if hasDefault {
+		return defaultVal
+	} else {
+		*err = ParameterError(name, fmt.Errorf("Missing required parmeter"))
+		return 0
+	}
+}
+
+func FloatParam(params map[string]string, name string, defaultVal float64, hasDefault bool, err *error) float64 {
+	if *err != nil {
+		return 0
+	}
+	strVal, ok := params[name]
+	if ok {
+		parsed, parseErr := strconv.ParseFloat(strVal, 64)
+		if parseErr != nil {
+			*err = ParameterError(name, parseErr)
+		}
+		return parsed
+	} else if hasDefault {
+		return defaultVal
+	} else {
+		*err = ParameterError(name, fmt.Errorf("Missing required parmeter"))
+		return 0
+	}
 }
 
 func (b *PipelineBuilder) RegisterAnalysisParamsErr(name string, setupPipeline AnalysisFunc, description string, requiredParams []string, optionalParams ...string) {
