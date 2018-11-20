433dde264e81e0047a0a9379e7d80d8235ddd265
Anton Gulenko <anton.gulenko@googlemail.com>
2018-11-20 16:03:18 +0100

    Small changes to the plugin build script

--- plugin/bitflow-plugin-mock/build.sh
+++ plugin/bitflow-plugin-mock/build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 # A plugin can be loaded using the -p switch of bitflow-pipeline:
 #  bitflow-pipeline -p bitflow-plugin-mock
@@ -12,7 +13,7 @@ echo "Building go-bitflow-pipeline..."
 go install github.com/bitflow-stream/go-bitflow-pipeline/...
 
 # Now build the plugin (use the name of the folder as binary target)
-echo "Building bitflow-plugin-mock..."
 home=`dirname $(readlink -e $0)`
 plugin_name=$(basename "$home")
+echo "Building ${plugin_name}..."
 cd "$home" && go build -buildmode=plugin -o "$plugin_name" .
