d4f99c661950bde6637c58f127b48bf09dde6796
Anton Gulenko <anton.gulenko@googlemail.com>
2020-04-01 11:05:51 +0200

    Failing Docker sanity check with empty stdout

--- build/test-image.sh
+++ build/test-image.sh
@@ -4,5 +4,5 @@ test $# = 1 || { echo "Need 1 parameter: image tag to test"; exit 1; }
 IMAGE="bitflowstream/bitflow-pipeline"
 TAG="$1"
 
-# Sanity check: image starts, outputs valid JSON, and terminates.
-docker run "$IMAGE:$TAG" -json-capabilities | jq . > /dev/null
+# Sanity check: image starts, outputs valid JSON, and terminates. Empty output also results in a non-zero exit status of jq.
+docker run "$IMAGE:$TAG" -json-capabilities | tee /dev/stderr | jq -ne inputs > /dev/null
